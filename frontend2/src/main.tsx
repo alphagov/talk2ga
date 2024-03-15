9a35a016c6ed23d843df9844a63dbca5a612639a
Guilhem Forey <guilhem.forey@hotmail.fr>
2024-03-15 16:51:46 +0000

    feat: created frontend2. UI working with LLM

---
+++ frontend2/src/main.tsx
@@ -0,0 +1,17 @@
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+// import App2 from "./App2.tsx";
+// @ts-ignore
+import { initAll } from "./govuk-frontend.min.js";
+
+initAll();
+
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import relativeDate from "dayjs/plugin/relativeTime";
+
+dayjs.extend(relativeDate);
+dayjs.extend(utc);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+// ReactDOM.createRoot(document.getElementById("root")!).render(<App2 />);
---
+++ frontend2/vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    outDir: "../backend/src/webapp/static",
+    rollupOptions: {
+      external: ['/javascripts/govuk-frontend.min.js']
+    }
+  },
+  base: "/static",
+  plugins: [svgr(), react()],
+  server: {
+    proxy: {
+      "^.*/(config_schema|input_schema|stream_log|feedback)(/[a-zA-Z0-9-]*)?$": {
+        target: "http://127.0.0.1:80",
+        changeOrigin: true,
+        rewrite: (path) => path.replace("/static", ""),
+      },
+    },
+  },
+});
