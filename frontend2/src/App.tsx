5d8950fc1235c8bf09af0bed093198bd43a8b25f
Guilhem Forey <guilhem.forey@hotmail.fr>
2024-03-20 08:45:45 +0000

    feat(fe): add typewriter animation

--- frontend2/src/App.tsx
+++ frontend2/src/App.tsx
@@ -5,8 +5,11 @@ import { useStreamLog } from "./useStreamLog";
 import { useAppStreamCallbacks } from "./useStreamCallback";
 import { str } from "./utils/str";
 import { StreamOutput, streamOutputToString } from "./components/StreamOutput";
-import { NotSatisfiedDetailsPayload, useQuestions } from "./useQuestionAnalytics";
-import Feedback from "./components/Feedback"
+import {
+  NotSatisfiedDetailsPayload,
+  useQuestions,
+} from "./useQuestionAnalytics";
+import Feedback from "./components/Feedback";
 
 function QuestionInput({
   handleSubmitQuestion,
@@ -82,6 +85,30 @@ function QuestionInput({
   );
 }
 
+const Typewriter = ({ text, delay }) => {
+  const [currentText, setCurrentText] = useState("");
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    if (currentIndex < text.length) {
+      const timeout = setTimeout(() => {
+        setCurrentText((prevText) => prevText + text[currentIndex]);
+        setCurrentIndex((prevIndex) => prevIndex + 1);
+      }, delay);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [currentIndex, delay, text]); // Depend on currentIndex, delay, and text for re-render
+
+  return <span>{currentText}</span>;
+};
+
+const Loading = () => (
+  <div className="govuk-inset-text">
+    <Typewriter text="Thinking about it 🤔... Writing some SQL 💻... Running some queries 🏃‍♂️... Crafting an answer ✍️..." delay={20} />
+  </div>
+);
+
 function Playground() {
   const [isStreaming, setIsStreaming] = useState(false);
   const [showLogs, setShowLogs] = useState(false);
@@ -90,8 +117,14 @@ function Playground() {
 
   const { context, callbacks } = useAppStreamCallbacks();
   const { startStream, stopStream, latest } = useStreamLog(callbacks);
-  const { recordQuestion, recordQuestionCompletion, recordFeedbackSatisfied, recordFeedbackNotSatisfied, recordFeedbackNotSatisfiedDetails, currentQuestionId } =
-    useQuestions();
+  const {
+    recordQuestion,
+    recordQuestionCompletion,
+    recordFeedbackSatisfied,
+    recordFeedbackNotSatisfied,
+    recordFeedbackNotSatisfiedDetails,
+    currentQuestionId,
+  } = useQuestions();
 
   const showLogsRef = useRef<(() => void) | null>(null);
   showLogsRef.current = () => {
@@ -112,7 +145,8 @@ function Playground() {
     context.current.onStart["recordQuestion"] = ({ input }) =>
       recordQuestion(input as string);
     context.current.onStart["setIsStreaming"] = () => setIsStreaming(true);
-    context.current.onStart["setHasCompletedFalse"] = () => setHasCompleted(false)
+    context.current.onStart["setHasCompletedFalse"] = () =>
+      setHasCompleted(false);
 
     // OnSuccess callbacks
     context.current.onSuccess["recordSuccess"] = () =>
@@ -133,23 +167,29 @@ function Playground() {
       recordQuestionCompletion(currentQuestionId, { succeeded: false });
   }, [latest, latest?.logs, latest?.final_output, currentQuestionId]);
 
-
   const onSatisfiedFeedback = () => {
-    currentQuestionId && recordFeedbackSatisfied(currentQuestionId)
-  }
-  
+    currentQuestionId && recordFeedbackSatisfied(currentQuestionId);
+  };
+
   const onNotSatisfiedFeedback = () => {
-    currentQuestionId && recordFeedbackNotSatisfied(currentQuestionId)
-  }
+    currentQuestionId && recordFeedbackNotSatisfied(currentQuestionId);
+  };
 
-  const onNotSatisfiedFeedbackDetailsSubmit = ({feedbackText, feedbackSql}: NotSatisfiedDetailsPayload) => { 
-    currentQuestionId && recordFeedbackNotSatisfiedDetails(currentQuestionId, {feedbackText, feedbackSql})
-  }
+  const onNotSatisfiedFeedbackDetailsSubmit = ({
+    feedbackText,
+    feedbackSql,
+  }: NotSatisfiedDetailsPayload) => {
+    currentQuestionId &&
+      recordFeedbackNotSatisfiedDetails(currentQuestionId, {
+        feedbackText,
+        feedbackSql,
+      });
+  };
 
+  const isLoading = isStreaming && !hasCompleted;
 
   return (
     <>
-      <p>ID: {currentQuestionId || "null"}</p>
       <QuestionInput
         handleSubmitQuestion={startStream}
         handleStopStreaming={stopStream}
@@ -157,10 +197,21 @@ function Playground() {
         toggleShowLogs={showLogsRef.current}
         showLogs={showLogs}
       />
-      {hasCompleted &&
-        <StreamOutput>{streamOutputToString(latest.streamed_output)}</StreamOutput>
-      }
-      {hasCompleted && <Feedback handleSatisfiedFeedback={onSatisfiedFeedback} handleNotSatisfiedFeedback={onNotSatisfiedFeedback} handleNotSatisfiedFeedbackFormSubmit={onNotSatisfiedFeedbackDetailsSubmit} />}
+      {isLoading && <Loading />}
+      {hasCompleted && (
+        <StreamOutput>
+          {streamOutputToString(latest.streamed_output)}
+        </StreamOutput>
+      )}
+      {hasCompleted && (
+        <Feedback
+          handleSatisfiedFeedback={onSatisfiedFeedback}
+          handleNotSatisfiedFeedback={onNotSatisfiedFeedback}
+          handleNotSatisfiedFeedbackFormSubmit={
+            onNotSatisfiedFeedbackDetailsSubmit
+          }
+        />
+      )}
       {showLogs &&
         latest &&
         latest.logs &&
