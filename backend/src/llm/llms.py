90f95d3a193024e50c0885b313fc431a3a01f743
Guilhem Forey <guilhem.forey@digital.cabinet-office.gov.uk>
2024-04-25 15:18:10 +0100

    fix(llm): increase response token limit to avoid truncating queries

--- backend/src/llm/llms.py
+++ backend/src/llm/llms.py
@@ -2,5 +2,7 @@ from langchain_google_vertexai import VertexAI
 from llm.config import GCP_PROJECT
 
 chat_bison = VertexAI(model_name="text-bison", project=GCP_PROJECT)
-code_bison_6k = VertexAI(model_name="code-bison@002", project=GCP_PROJECT)
+code_bison_6k = VertexAI(
+    model_name="code-bison@002", project=GCP_PROJECT, max_output_tokens=2048
+)
 gemini_pro = VertexAI(model_name="gemini-pro", project=GCP_PROJECT)
