d26d8d49202760001dcf583bfdbff13e4b728712
Guilhem Forey <guilhem.forey@digital.cabinet-office.gov.uk>
2024-04-26 08:53:44 +0100

    feat(llm): upgrade the code and text models

--- backend/src/llm/llms.py
+++ backend/src/llm/llms.py
@@ -1,7 +1,9 @@
 from langchain_google_vertexai import VertexAI
 from llm.config import GCP_PROJECT
 
-chat_bison = VertexAI(model_name="text-bison", project=GCP_PROJECT)
+text_bison = VertexAI(
+    model_name="text-bison@002", project=GCP_PROJECT, max_output_tokens=2048
+)
 code_bison_6k = VertexAI(
     model_name="code-bison@002", project=GCP_PROJECT, max_output_tokens=2048
 )
