a5d1cab057de7fc7a4ee80cee16637af6689fbc1
Guilhem Forey <guilhem.forey@digital.cabinet-office.gov.uk>
2024-05-29 17:20:37 +0100

    chore: remove debugging logs

--- backend/src/llm/whole_chain.py
+++ backend/src/llm/whole_chain.py
@@ -198,7 +198,6 @@ def selected_sql_passthrough(sql):
 @log_error_to_analytics
 @_observe()
 def whole_chain(json_input: str, config: dict[str, any], test_callback=None):
-    print("DEBUGGING whole_chain start")
     input = json.loads(json_input)
     original_question = input.get("question")
     question_id = config.get("question_id")
@@ -208,7 +207,6 @@ def whole_chain(json_input: str, config: dict[str, any], test_callback=None):
     response_object = None
 
     # sql, was_corrected = generate_sql_from_question(question, date_range, question_id)
-    print("DEBUGGING whole_chain initialised")
     if appconfig.FF_PROMPT_REFINEMENT_ENABLED:
         question = refine_question(original_question)
     else:
@@ -216,15 +214,11 @@ def whole_chain(json_input: str, config: dict[str, any], test_callback=None):
 
     while response_object is None and count_retries < max_tries:
         try:
-            print("DEBUGGING whole_chain first try")
             sql, was_corrected = generate_sql_from_question(question, date_range, question_id)
-            print("DEBUGGING whole_chain SQL generated")
             # Running the SQL though a passthrough just to get the sql from the stream log in the frontend
             # TODO: create API endpoints to record / get the SQL by question ID instead of using the stream log
             sql = selected_sql_passthrough.invoke(sql)
-            print("DEBUGGING whole_chain after passthrough")
             response_object = query_sql(sql, question_id)
-            print("DEBUGGING whole_chain SQL queried")
         except Exception as e:
             count_retries += 1
             print(f"\nquery_sql failed. Retrying {count_retries}/{max_tries}...\n")
