e1f863b8c18c7e13c154fff7b837aead9472ce48
Guilhem Forey <guilhem.forey@digital.cabinet-office.gov.uk>
2024-07-11 15:56:07 +0100

    chore: remove /static from browser URL paths when developing locally

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,23 +3,27 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    outDir: '../backend/src/webapp/static',
-    rollupOptions: {
-      external: ['/javascripts/govuk-frontend.min.js'],
+export default defineConfig(({ command }) => {
+  const isProduction = command === 'build';
+  console.log({ isProduction });
+  return {
+    build: {
+      outDir: '../backend/src/webapp/static',
+      rollupOptions: {
+        external: ['/javascripts/govuk-frontend.min.js'],
+      },
     },
-  },
-  base: '/static',
-  plugins: [svgr(), react()],
-  server: {
-    proxy: {
-      '^.*/(question|config_schema|input_schema|stream_log|feedback|custom_chain)(/[a-zA-Z0-9-]*)?$':
-        {
-          target: 'http://127.0.0.1:80',
-          changeOrigin: true,
-          rewrite: (path) => path.replace('/static', ''),
-        },
+    base: isProduction ? '/static' : '/',
+    plugins: [svgr(), react()],
+    server: {
+      proxy: {
+        '^.*/(question|config_schema|input_schema|stream_log|feedback|custom_chain)(/[a-zA-Z0-9-]*)?$':
+          {
+            target: 'http://127.0.0.1:80',
+            changeOrigin: true,
+            rewrite: (path) => path.replace('/static', ''),
+          },
+      },
     },
-  },
+  };
 });
