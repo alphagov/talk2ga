3c0465001f2a880659fed4dcea963ca28967ca25
Guilhem Forey <guilhem.forey@digital.cabinet-office.gov.uk>
2024-07-20 11:14:23 +0100

    feat: first draft of the response new design

--- frontend/src/components/MainAnswer.tsx
+++ frontend/src/components/MainAnswer.tsx
@@ -1,33 +1,73 @@
+import { FrontendDateRange } from '../types';
+
 function splitByLineReturns(input: string) {
   return input.split(/\r\n|\r|\n/);
 }
 
-export function MainAnswer({ text }: { text: string }) {
+function formatDate(date: Date): string {
+  const options: Intl.DateTimeFormatOptions = {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  };
+  return date.toLocaleDateString('en-GB', options);
+}
+
+export function MainAnswer({
+  text,
+  dateRange,
+}: {
+  text: string;
+  dateRange: FrontendDateRange;
+}) {
+  const dateString = `${formatDate(dateRange[0])} to ${formatDate(
+    dateRange[1],
+  )}`;
   return (
     <div
-      className="govuk-notification-banner"
+      className="main-answer-container govuk-!-static-padding-5"
       role="region"
-      aria-labelledby="govuk-notification-banner-title"
-      data-module="govuk-notification-banner"
     >
-      <div className="govuk-notification-banner__header">
-        <h2
-          className="govuk-notification-banner__title"
-          id="govuk-notification-banner-title"
-        >
-          Answer
-        </h2>
-      </div>
-      <div className="govuk-notification-banner__content">
-        <p className="govuk-notification-banner__heading answer-banner">
-          {splitByLineReturns(text).map((line, i) => (
-            <span key={i}>
-              {line}
-              <br />
-            </span>
-          ))}
+      <h1 className="govuk-heading-m govuk-!-static-margin-bottom-4">
+        <span className="govuk-caption-m govuk-!-static-margin-bottom-4">
+          {dateString}
+        </span>
+        Top 5 countries with the most users:
+      </h1>
+      {splitByLineReturns(text).map((line, i) => (
+        <p key={i} className="govuk-body">
+          {line}
         </p>
+      ))}
+      <div className="govuk-warning-text">
+        <span className="govuk-warning-text__icon" aria-hidden="true">
+          !
+        </span>
+        <strong className="govuk-warning-text__text">
+          <span className="govuk-visually-hidden">Warning</span>
+          Verify the result before sharing
+        </strong>
       </div>
+      <details className="govuk-details">
+        <summary className="govuk-details__summary">
+          <span className="govuk-details__summary-text">Generated SQL</span>
+        </summary>
+        <div className="govuk-details__text">
+          We need to know your nationality so we can work out which elections
+          you’re entitled to vote in. If you cannot provide your nationality,
+          you’ll have to send copies of identity documents through the post.
+        </div>
+      </details>
+      <details className="govuk-details">
+        <summary className="govuk-details__summary">
+          <span className="govuk-details__summary-text">SQL Explained</span>
+        </summary>
+        <div className="govuk-details__text">
+          We need to know your nationality so we can work out which elections
+          you’re entitled to vote in. If you cannot provide your nationality,
+          you’ll have to send copies of identity documents through the post.
+        </div>
+      </details>
     </div>
   );
 }
