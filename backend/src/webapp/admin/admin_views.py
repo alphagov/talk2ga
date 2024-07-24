d076dcdc3ec056e4a5520dba2166628d31e0f448
Guilhem Forey <guilhem.forey@digital.cabinet-office.gov.uk>
2024-07-24 17:23:20 +0100

    feat(b): admin: enhance with col labels, pagination, sorting defaults

--- backend/src/webapp/admin/admin_views.py
+++ backend/src/webapp/admin/admin_views.py
@@ -10,17 +10,24 @@ def convert_date_to_readable(model, attribute) -> str:
 
 
 class QuestionAdmin(ModelView, model=Question):
+    page_size = 100
+    page_size_options = [10, 25, 50, 100, 200, 500]
+
+    column_default_sort = ("created_at", True)
+    column_sortable_list = ["created_at", "succeeded", "username", "user_email", "has_feedback", "is_feedback_positive"]
+
     can_create = False
     can_edit = False
     can_delete = False
 
     column_list = [
         Question.created_at,
-        Question.text,
         Question.succeeded,
+        Question.text,
+        Question.final_output,
+        Question.executed_sql_query,
         Question.error_msg,
         # Question.logs_json,
-        Question.final_output,
         Question.duration,
         Question.username,
         Question.user_email,
@@ -29,9 +36,26 @@ class QuestionAdmin(ModelView, model=Question):
         Question.feedback_text,
         Question.suggested_sql_correction,
         Question.explanations,
-        Question.executed_sql_query,
         Question.generated_sql_queries,
     ]
+
+    column_labels = {
+        Question.created_at: "Created At",
+        Question.text: "Question & Date Range",
+        Question.succeeded: "Succeeded",
+        Question.error_msg: "Error Message",
+        Question.logs_json: "Logs JSON",
+        Question.final_output: "Given Answer",
+        Question.duration: "Duration",
+        Question.username: "Username",
+        Question.user_email: "User Email",
+        Question.has_feedback: "Has Feedback",
+        Question.is_feedback_positive: "Is Feedback Positive",
+        Question.feedback_text: "User Text Feedback",
+        Question.suggested_sql_correction: "Suggested SQL Correction",
+        Question.explanations: "SQL Explanations",
+        Question.executed_sql_query: "SQL Query",
+    }
 
     column_formatters = {Question.created_at: convert_date_to_readable}
 
