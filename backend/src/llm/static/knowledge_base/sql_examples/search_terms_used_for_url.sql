50f20f4fbf18003acdb4d33e77c6194e82903d04
Guilhem Forey <guilhem.forey@digital.cabinet-office.gov.uk>
2024-08-22 18:49:51 +0100

    feat: can answer questions on search terms

---
+++ backend/src/llm/static/knowledge_base/sql_examples/search_terms_used_for_url.sql
@@ -0,0 +1,20 @@
+-- Question: What search terms did users enter to reach a specific page?
+-- Description: Retrieves and ranks search terms users entered that led them to a specific page using the `select_item` event.
+-- Explanation: Filters by the `select_item` event on the search results page and the destination page URL. Splits the `search_term` into individual words, counts occurrences, and returns the most common terms.
+
+-- SQL
+SELECT
+  word,
+  COUNT(*) AS word_count
+FROM
+  `ga4-analytics-352613.flattened_dataset.flattened_daily_ga_data_20240821`,
+  UNNEST(SPLIT(search_term, ' ')) AS word
+WHERE
+  event_name = 'select_item'
+  AND cleaned_page_location = '/search/all'
+  AND link_url = '/sign-in-universal-credit'
+GROUP BY
+  word
+ORDER BY
+  word_count DESC
+LIMIT 10;
